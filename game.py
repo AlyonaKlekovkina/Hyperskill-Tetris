55c24e1977d97ef5c176c9a09aac4c0e1cf90fed
AlyonaKlekovkina <like.the.sunshine.17@gmail.com>
2025-01-14 12:14:53 +0100

    stage 3 - create the boundaries (refactored previous code, now I like it finally)

--- game.py
+++ game.py
@@ -2,13 +2,26 @@
 import numpy as np
 
 
-def create_first_grid(width, height):
+def create_first_grid(dropped_pieces, width, height):
+    grid_row = 0
+    piece_count = 0
+    the_piece = dropped_pieces[0]
     outer = []
+    if len(dropped_pieces) >= 2:
+        for k in range(1, len(dropped_pieces)):
+            the_piece = dropped_pieces[k]
+            #print(the_piece)
     for i in range(height):
         inner = []
         for j in range(width):
-            inner.append('-')
+            if the_piece[piece_count] == j + grid_row and the_piece[piece_count] != 0:
+                inner.append('0')
+                if piece_count < (len(the_piece) - 1):
+                    piece_count += 1
+            else:
+                inner.append('-')
         outer.append(inner)
+        grid_row += 10
     for j in outer:
         print(" ".join(np.array(j)))
     print()
@@ -16,7 +29,7 @@ def create_first_grid(width, height):
 
 
 def interpret_input_piece():
-    shape_input = input()
+    shape_input = input('select a shape: ')
     shapes = {
         'O': [[4, 5, 14, 15]],
         'I': [[4, 14, 24, 34], [3, 4, 5, 6]],
@@ -51,13 +64,22 @@ def put_piece_on_grid(grid, piece):
         print(" ".join(np.array(j)))
 
 
-def check_boarders(piece, step, width, height):
-    if (piece[-1] + step + width) >= (width * height):
-        return 'down'
-    elif (piece[0] + step) % width == 0 and (piece[-1] + step) < (width * height):
-        return 'left'
-    elif (piece[-1] + step) % width == (width - 1) and (piece[-1] + step) < (width * height):
-        return 'right'
+def check_boarders_down(piece, step, width, height):
+    for i in piece:
+        if i + step + width >= (width * height):
+            return 'down'
+
+
+def check_boarders_left(piece, step, width):
+    for i in piece:
+        if (i + step) % width == 0:
+            return 'left'
+
+
+def check_boarders_right(piece, step, width):
+    for i in piece:
+        if (i + step) % width == (width - 1):
+            return 'right'
 
 
 def moved_piece(step, shape):
@@ -70,11 +92,12 @@ def moved_piece(step, shape):
 dimensions = input().split()
 board_width = int(dimensions[0])
 board_height = int(dimensions[1])
-grid = create_first_grid(board_width, board_height)
+dropped_pieces = [[0, 0, 0, 0]]
 
 while True:
-    commands = ['piece', 'rotate', 'left', 'right', 'down', 'break', 'exit']
-    command = input()
+    grid = create_first_grid(dropped_pieces, board_width, board_height)
+    commands = ['piece', 'break', 'exit']
+    command = input('enter a command: ')
     if command not in commands:
         print('Such command does not exist')
     else:
@@ -86,50 +109,26 @@ while True:
             shape = interpret_input_piece()
             put_piece_on_grid(grid, shape[count])
             while True:
+                movements = ['rotate', 'left', 'right', 'down']
                 move = input()
-                checked_boarders = check_boarders(shape[count], step, board_width, board_height)
-                if move == 'left':
-                    if checked_boarders != 'left' and checked_boarders != 'down':
-                        step += board_width - 1
+                if move in movements:
+                    if check_boarders_down(shape[count], step, board_width, board_height) == 'down':
                         shifted_piece = moved_piece(step, shape[count])
-                        put_piece_on_grid(grid, shifted_piece)
-                    elif checked_boarders == 'left':
-                        step += board_width
-                        shifted_piece = moved_piece(step, shape[count])
-                        put_piece_on_grid(grid, shifted_piece)
-                    if checked_boarders == 'down':
-                        #piece should be printed on grid and program moves to the following piece
+                        dropped_pieces.append(shifted_piece)
                         break
-                if move == 'right':
-                    if checked_boarders != 'right' and checked_boarders != 'down':
-                        step += board_width + 1
+                    else:
+                        if move == 'left' and check_boarders_left(shape[count], step, board_width) != 'left':
+                            step += board_width - 1
+                        elif move == 'right' and check_boarders_right(shape[count], step, board_width) != 'right':
+                            step += board_width + 1
+                        elif move == 'rotate':
+                            if (len(shape) - 1) > count:
+                                count += 1
+                            elif (len(shape) - 1) == count:
+                                count = 0
+                            if check_boarders_right(shape[count], step, board_width) != 'right' and check_boarders_left(shape[count], step, board_width) != 'left':
+                                step += board_width
+                        else:
+                            step += board_width
                         shifted_piece = moved_piece(step, shape[count])
                         put_piece_on_grid(grid, shifted_piece)
-                    if checked_boarders == 'right':
-                        step += board_width
-                        shifted_piece = moved_piece(step, shape[count])
-                        put_piece_on_grid(grid, shifted_piece)
-                    if checked_boarders == 'down':
-                        #piece should be printed on grid and program moves to the following piece
-                        break
-                elif move == 'down':
-                    if checked_boarders != 'down':
-                        step += board_width
-                        shifted_piece = moved_piece(step, shape[count])
-                        put_piece_on_grid(grid, shifted_piece)
-                    elif checked_boarders == 'down':
-                        # piece should be printed on grid and program moves to the following piece
-                        break
-                elif move == 'rotate':
-                    if (len(shape) - 1) > count:
-                        count += 1
-                    elif (len(shape) - 1) == count:
-                        count = 0
-                    checked_boarders = check_boarders(shape[count], step, board_width, board_height)
-                    if checked_boarders != 'right' and checked_boarders != 'down' and checked_boarders != 'left':
-                        step += board_width
-                        shifted_piece = moved_piece(step, shape[count])
-                        put_piece_on_grid(grid, shifted_piece)
-                    elif checked_boarders == 'down':
-                        # piece should be printed on grid and program moves to the following piece
-                        break
