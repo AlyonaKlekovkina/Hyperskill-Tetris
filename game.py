aadbd3f166481f753f2c6409bc2a247095da032e
AlyonaKlekovkina <like.the.sunshine.17@gmail.com>
2025-01-16 15:16:54 +0100

    stage 3 - create the boundaries (refactored previous code, hits the floor, hits the previous piece, saves fallen blocks)

--- game.py
+++ game.py
@@ -2,34 +2,21 @@
 import numpy as np
 
 
-def create_first_grid(dropped_pieces, width, height):
-    grid_row = 0
-    piece_count = 0
-    the_piece = dropped_pieces[0]
+def create_first_grid(width, height):
     outer = []
-    if len(dropped_pieces) >= 2:
-        for k in range(1, len(dropped_pieces)):
-            the_piece = dropped_pieces[k]
-            #print(the_piece)
     for i in range(height):
         inner = []
         for j in range(width):
-            if the_piece[piece_count] == j + grid_row and the_piece[piece_count] != 0:
-                inner.append('0')
-                if piece_count < (len(the_piece) - 1):
-                    piece_count += 1
-            else:
-                inner.append('-')
+            inner.append('-')
         outer.append(inner)
-        grid_row += 10
-    for j in outer:
-        print(" ".join(np.array(j)))
+    for k in outer:
+        print(" ".join(np.array(k)))
     print()
     return outer
 
 
 def interpret_input_piece():
-    shape_input = input('select a shape: ')
+    shape_input = input('Select a shape: ')
     shapes = {
         'O': [[4, 5, 14, 15]],
         'I': [[4, 14, 24, 34], [3, 4, 5, 6]],
@@ -60,26 +47,30 @@ def put_piece_on_grid(grid, piece):
                 grid_line.append('-')
         grid_height.append(grid_line)
         grid_row += 10
-    for j in grid_height:
-        print(" ".join(np.array(j)))
+    return grid_height
 
 
-def check_boarders_down(piece, step, width, height):
-    for i in piece:
-        if i + step + width >= (width * height):
-            return 'down'
+def violates_bottom_boundary(piece, step, width, height, dropped_pieces):
+    all_dropped_positions = sorted(np.concatenate(dropped_pieces)) if dropped_pieces else []
+    for pos in piece:
+        future_pos = pos + step + width
+        if future_pos >= width * height:
+            return True  # Hits the floor
+        if future_pos in all_dropped_positions:
+            return True  # Collides with another piece
+    return False
 
 
-def check_boarders_left(piece, step, width):
+def violates_left_boundary(piece, step, width):
     for i in piece:
         if (i + step) % width == 0:
-            return 'left'
+            return True
 
 
-def check_boarders_right(piece, step, width):
+def violates_right_boundary(piece, step, width):
     for i in piece:
         if (i + step) % width == (width - 1):
-            return 'right'
+            return True
 
 
 def moved_piece(step, shape):
@@ -89,15 +80,47 @@ def moved_piece(step, shape):
     return moved_piece
 
 
+def put_dropped_pieces_on_grid(dropped_pieces, width, height):
+    sorted_pieces = sorted(np.concatenate(dropped_pieces))
+    grid_row = 0
+    piece_count = 0
+    grid_height = []
+    for i in range(height):
+        grid_line = []
+        for j in range(width):
+            if sorted_pieces[piece_count] == j + grid_row:
+                grid_line.append('0')
+                if piece_count < (len(sorted_pieces) - 1):
+                    piece_count += 1
+            else:
+                grid_line.append('-')
+        grid_height.append(grid_line)
+        grid_row += 10
+    return grid_height
+
+
+def merge_two_grids(moving, static):
+    outer = []
+    for i, j in zip(moving, static):
+        inner = []
+        for k in range(len(i)):
+            if i[k] == '0' or j[k] == '0':
+                inner.append('0')
+            elif i[k] == '-' or j[k] == '-':
+                inner.append('-')
+        outer.append(inner)
+    return outer
+
+
 dimensions = input().split()
 board_width = int(dimensions[0])
 board_height = int(dimensions[1])
-dropped_pieces = [[0, 0, 0, 0]]
+dropped_pieces = []
+grid = create_first_grid(board_width, board_height)
 
 while True:
-    grid = create_first_grid(dropped_pieces, board_width, board_height)
     commands = ['piece', 'break', 'exit']
-    command = input('enter a command: ')
+    command = input('Enter a command "piece", "break" or "exit": ')
     if command not in commands:
         print('Such command does not exist')
     else:
@@ -107,28 +130,39 @@ while True:
             step = 0
             count = 0
             shape = interpret_input_piece()
-            put_piece_on_grid(grid, shape[count])
+            first_piece = put_piece_on_grid(grid, shape[count])
+            for f in first_piece:
+                print(" ".join(np.array(f)))
             while True:
                 movements = ['rotate', 'left', 'right', 'down']
-                move = input()
+                move = input("Select movement: ")
                 if move in movements:
-                    if check_boarders_down(shape[count], step, board_width, board_height) == 'down':
+                    if violates_bottom_boundary(shape[count], step, board_width, board_height, dropped_pieces):
                         shifted_piece = moved_piece(step, shape[count])
                         dropped_pieces.append(shifted_piece)
+                        grid_with_dropped = put_dropped_pieces_on_grid(dropped_pieces, board_width, board_height)
+                        for i in grid_with_dropped:
+                            print(" ".join(np.array(i)))
                         break
                     else:
-                        if move == 'left' and check_boarders_left(shape[count], step, board_width) != 'left':
+                        if move == 'left' and not violates_left_boundary(shape[count], step, board_width):
                             step += board_width - 1
-                        elif move == 'right' and check_boarders_right(shape[count], step, board_width) != 'right':
+                        elif move == 'right' and not violates_right_boundary(shape[count], step, board_width):
                             step += board_width + 1
                         elif move == 'rotate':
                             if (len(shape) - 1) > count:
                                 count += 1
                             elif (len(shape) - 1) == count:
                                 count = 0
-                            if check_boarders_right(shape[count], step, board_width) != 'right' and check_boarders_left(shape[count], step, board_width) != 'left':
-                                step += board_width
                         else:
                             step += board_width
                         shifted_piece = moved_piece(step, shape[count])
-                        put_piece_on_grid(grid, shifted_piece)
+                        moving_piece = put_piece_on_grid(grid, shifted_piece)
+                        if len(dropped_pieces) == 0:
+                            for h in moving_piece:
+                                print(" ".join(np.array(h)))
+                        else:
+                            fallen_pieces = put_dropped_pieces_on_grid(dropped_pieces, board_width, board_height)
+                            merged_grid = merge_two_grids(moving_piece, fallen_pieces)
+                            for m in merged_grid:
+                                print(" ".join(np.array(m)))
